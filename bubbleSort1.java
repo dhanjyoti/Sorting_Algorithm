742dd09dea0c7f140c172b450e07f0a0cdc4eef4
Dhanjyoti Rabha <115441418+dhanjyoti@users.noreply.github.com>
2023-05-11 15:50:51 +0530

    Update bubbleSort1.java
    removing from 1st for(i<n-1) to just n & for(j<n-i-1) to just n-1 making the function simple and sorter

--- bubbleSort1.java
+++ bubbleSort1.java
@@ -23,8 +23,8 @@ public class BubbleSort1{
 
     public static void bubbleSort(int[] arr){
         int n = arr.length;
-        for(int i=0; i<n-1; i++){
-            for(int j=0; j<n-i-1; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-1; j++){
                 if(arr[j] > arr[j+1]){
                     // Swap arr[j] and arr[j-1]
                     int temp = arr[j];
